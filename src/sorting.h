a66f362627e2f91ede113fce126fe466766e1132
Joe Singleton <joesingo@gmail.com>
2017-01-24 16:52:34 +0000

    Determine algorithm to use and data type (e.g. best, wost, average) from command line arguments

--- src/sorting.h
+++ src/sorting.h
@@ -1,6 +1,11 @@
 #ifndef SORTING_H
 #define SORTING_H
 
+typedef enum {
+    INSERTION_SORT,
+    COUNTING_SORT
+} SortingAlgorithm;
+
 void insertion_sort(float **array, int size);
 
 #endif
--- src/sorting_test.c
+++ src/sorting_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "sorting.h"
 #include "data_gen.h"
@@ -12,8 +13,82 @@ int START_SIZE = 100;
 int SIZE_STEP = 100;
 int NUM_SIZES = 30;
 
+char *PROGRAM_NAME;
+
+/*
+ * An enum to hold the different scenarios for performing the test
+ */
+typedef enum {
+    BEST_CASE,
+    WORST_CASE,
+    AVERAGE_CASE
+} Scenario;
+
+/*
+ * Print an error message on stderr
+ */
+void print_error(char *message) {
+    fprintf(stderr, "%s: %s\n", PROGRAM_NAME, message);
+}
+
+/*
+ * Print usage text to stdout
+ */
+void print_usage() {
+    printf("Usage: %s (insertion | counting) (best | worst | average)\n",
+           PROGRAM_NAME);
+}
+
+/*
+ * Validate the provided command line arguments and set the scenario and
+ * algorithm using the pointers provided
+ */
+void get_args(int argc, char **argv, Scenario *scenario, SortingAlgorithm *alg) {
+    // Check the correct number of arguments have been provided
+    if (argc < 3) {
+        print_error("Invalid arguments");
+        print_usage();
+        exit(1);
+    }
+
+    // Check the scenario (best/worst/average) is valid
+    if (strcmp(argv[2], "best") == 0) {
+        *scenario = BEST_CASE;
+    }
+    else if (strcmp(argv[2], "worst") == 0) {
+        *scenario = WORST_CASE;
+    }
+    else if (strcmp(argv[2], "average") == 0) {
+        *scenario = AVERAGE_CASE;
+    }
+    else {
+        print_error("Invalid scenario");
+        print_usage();
+        exit(1);
+    }
+
+    // Check sorting algorithm is valid
+    if (strcmp(argv[1], "insertion") == 0) {
+        *alg = INSERTION_SORT;
+    }
+    else if (strcmp(argv[1], "counting") == 0) {
+        *alg = COUNTING_SORT;
+    }
+    else {
+        print_error("Invalid sorting algorithm");
+        print_usage();
+        exit(1);
+    }
+
+}
+
 void main(int argc, char **argv) {
     srandom(time(NULL));
+    PROGRAM_NAME = "sorting_test";
+
+    Scenario scenario;
+    SortingAlgorithm alg;
+    get_args(argc, argv, &scenario, &alg);
 
     // Set data gen parameters to default values
     DataGeneratorParams params;
@@ -21,8 +96,27 @@ void main(int argc, char **argv) {
     params.max = 100;
     params.ordering = RANDOM;
 
-    // TODO: Work out parameters based on sorting algorithm and scenario from
-    // command line args
+    void (*sorting_func)(float**, int);  // function pointer
+
+    // Set sorting_func and data gen params according to chosen algorithm
+    switch (alg) {
+
+        case INSERTION_SORT:
+            sorting_func = &insertion_sort;
+
+            if (scenario == BEST_CASE) {
+                params.ordering = SORTED;
+            }
+            else if (scenario == WORST_CASE) {
+                params.ordering = REVERSE_SORTED;
+            }
+            break;
+
+        case COUNTING_SORT:
+            print_error("Counting sort is not implemented yet");
+            exit(1);
+            break;
+    }
 
     // Caclulate the array sizes to use based on the values defined above
     int sizes[NUM_SIZES];
@@ -30,6 +124,7 @@ void main(int argc, char **argv) {
         sizes[i] = START_SIZE + i * SIZE_STEP;
     }
 
+    // Perform the actual test
     for (int i=0; i<NUM_SIZES; i++) {
 
         double total_time = 0;
@@ -40,7 +135,7 @@ void main(int argc, char **argv) {
 
             for (int k=0; k<TEST_REPETITIONS; k++) {
                 clock_t start = clock();
-                insertion_sort(&list, sizes[i]);
+                (*sorting_func)(&list, sizes[i]);
                 clock_t end = clock();
                 total_time += (double)(end - start) / CLOCKS_PER_SEC;
             }
